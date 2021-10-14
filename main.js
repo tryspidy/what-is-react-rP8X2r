c5bd6d7ffbb7725abfe9c52bd25992b1c7c61092
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-14 14:07:54 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+React is a JavaScript library for building user interfaces.
+
+It is maintained by Facebook and a community of individual developers and
+companies.
+
+React can be used as a base in the development of single-page or mobile
+applications.
